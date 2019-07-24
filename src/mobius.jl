998d8f40f8cd8c78b89c0ca3c414dc96fac2095e
Toby Driscoll <driscoll@udel.edu>
2019-07-24 13:54:14 -0400

    More documentation

--- src/mobius.jl
+++ src/mobius.jl
@@ -1,6 +1,10 @@
 abstract type AbstractMap end
 Line_Circle = Union{Line,Circle}
 
+"""
+	(type) Möbius
+Representation of a Möbius or bilinear transformation.
+"""
 struct Möbius <: AbstractMap
 	# interpreted as [a,b,c,d], where f(z)=(az+b)/(cz+d) 
 	coeff::SVector{4} 
