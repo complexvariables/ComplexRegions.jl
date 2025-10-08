e42b3a7cd65cb0534454fa953da76447c5f932ed
Toby Driscoll <driscoll@udel.edu>
2025-10-08 12:08:43 -0400

    clarify documentation string

--- src/simplyconnected.jl
+++ src/simplyconnected.jl
@@ -32,7 +32,9 @@ InteriorSimplyConnectedRegion(C::AbstractJordan) = interior(C)
 
 """
 	exterior(C)
-Construct the region exterior to the closed curve or path `C`. If `C` is bounded, the bounded enclosure is chosen regardless of the orientation of `C`; otherwise, the region "to the right" is the exterior.
+Construct the region exterior to the closed curve or path `C`. If `C` is bounded, the
+complement of the bounded enclosure is chosen regardless of the orientation of `C`; otherwise,
+the region "to the right" is the exterior.
 """
 function exterior(C::AbstractJordan{T}) where {T}
     if isfinite(C)
